fce264c91e745ddeda6c6f76b6e03b361c65956d
Shivam Sharma <shivamsharma022005@gmail.com>
2022-04-06 00:21:30 +0530

    new branch

--- app/src/main/java/com/shivam/new_buddy/DataRec.kt
+++
@@ -1,6 +0,0 @@
-package com.shivam.new_buddy
-data class DataRec(val image:Int,val textname:String,val textmsg:String, val time:String){
-
-
-
-}
--- app/src/main/java/com/shivam/new_buddy/message/SearchProfile.kt
+++ app/src/main/java/com/shivam/new_buddy/message/SearchProfile.kt
@@ -1,13 +1,17 @@
 
 package com.shivam.new_buddy.message
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
 import com.shivam.new_buddy.R
-
 class SearchProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_profile)
+        val btback=findViewById<RelativeLayout>(R.id.rl_return)
+        btback.setOnClickListener(){
+            onBackPressed()
+        }
     }
 }
+
